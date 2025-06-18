247675e22057c313113aaf71d06228f20031e7d9
intra-42 <anawintuanku@gmail.com>
2025-06-18 11:30:54 +0000

    57

---
+++ cell05/ex07/downcase_it.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("none")
+    else:
+        text = " ".join(sys.argv[1:])
+        print(text.lower())
+
+
+if __name__ == "__main__":
+    main()
